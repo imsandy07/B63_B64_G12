af370720596775019c80aa8bbc02c4479075673c
sandy <psaandheep7@gmail.com>
2025-03-27 10:57:11 -0600

    First commit

---
+++ g12_sp/g12_psc01/g01_psc01c1fcs/g12_3_console.py.py
@@ -0,0 +1,39 @@
+from salesmanager import view_sales, add_sales1, add_sales2, import_sales, import_all_sales, save_all_sales
+
+def display_title() -> None:
+    print("SALES DATA IMPORTER")
+
+def display_menu() -> None:
+    print("""
+COMMAND MENU
+view   - View all sales
+add1   - Add sales by typing sales, year, month, day, and region
+add2   - Add sales by typing sales, date (YYYY-MM-DD), and region
+import - Import sales from file
+menu   - Show menu
+exit   - Exit program
+""")
+
+def execute_command() -> None:
+    sales_list = import_all_sales()
+    display_title()
+    display_menu()
+
+    while True:
+        command = input("Please enter a command: ").lower()
+        if command == "view":
+            view_sales(sales_list)
+        elif command == "add1":
+            add_sales1(sales_list)
+        elif command == "add2":
+            add_sales2(sales_list)
+        elif command == "import":
+            import_sales(sales_list)
+        elif command == "menu":
+            display_menu()
+        elif command == "exit":
+            save_all_sales(sales_list)
+            print("Saved sales records.\nBye!")
+            break
+        else:
+            print("Invalid command. Please try again.")
---
+++ g12_sp/g12_psc01/g01_psc01c1fcs/g12_4_main.py.py
@@ -0,0 +1,4 @@
+from console import execute_command
+
+if __name__ == "__main__":
+    execute_command()
